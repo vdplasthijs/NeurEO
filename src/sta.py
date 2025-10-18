b3a0fa7ce92c46fd8499eeddda40427c6ac11ba7
Jacob <23357015+jbakermans@users.noreply.github.com>
2025-10-18 12:06:17 +0100

    Plot parameter distributions, load previous analyses
    Reorganise code to load previously calculated stas and fits, then plot the fitted parameters across features for each hypothesis.

--- src/sta.py
+++ src/sta.py
@@ -8,6 +8,7 @@ Created on Thu Oct  9 12:20:07 2025
 
 import data_utils as du
 import numpy as np
+import os
 from matplotlib import pyplot as plt
 from matplotlib import colormaps
 from matplotlib.patches import Rectangle
@@ -23,6 +24,8 @@ def gauss_2d(xy, amplitude, xo, yo, sigma_x, sigma_y, theta, offset):
     g = offset + amplitude*np.exp( - (a*((x-xo)**2) + 2*b*(x-xo)*(y-yo)
                             + c*((y-yo)**2)))
     return g.ravel()
+
+### LOAD DATA ###
 
 # Then collect from all patches the alpha and dyn data
 patches = 200
@@ -57,75 +60,140 @@ hypotheses = [(h - hyp_m[:,None,None])/hyp_std[:,None,None] for h in hypotheses]
 names = [k for k in du.create_cmap_dynamic_world().keys()] + ['dsm']
 
 # Extract relevant dimensions
-radius = 10 # pixels, excluding center pixel (so diameter = 2 * radius + 1)
+radius = 5 # pixels, excluding center pixel (so diameter = 2 * radius + 1)
 N_features = features[0].shape[0]
 N_pixels = features[0].shape[1]
 N_hypotheses = hypotheses[0].shape[0]
 
-# Run through all patches, collecting spike triggered averages for each feature for each hypothesis
-patch_stas = []
-for p, (hypothesis, feature) in enumerate(zip(hypotheses, features)):
-    print(f'Analysing patch {p+1} / {len(hypotheses)}')
-    
-    # Create empty region of interest maps: area around each pixel for each band
-    rois = np.full([N_hypotheses, N_pixels - 2 * radius, N_pixels - 2 * radius,
-                    radius * 2 + 1, radius * 2 + 1], np.nan)
-    
-    # Collect searchlight data for each pixel from all bands of current modality
-    for h, hyp in enumerate(hypothesis):
-        print(f'Copying hyp {h} / {len(hypotheses)}')
-        for row in range(radius, N_pixels - radius):
-            for col in range(radius, N_pixels - radius):
-                # Grab the relevant pixels from the band
-                rois[h, row - radius, col - radius, :, :] = \
-                    hyp[(row - radius):(row + radius + 1), (col - radius):(col + radius + 1)]
-                    
-    # Collect spike time averages for each band
-    stas = []
-    for h, hyp_rois in enumerate(rois):
-        print(f'Calculating spike triggered averages for hyp {h} / {len(rois)}')
-        # Then create the spike time average: multiply each roi by the pixel value of the feature pixel
-        hyp_stas = [hyp_rois * d[radius:(N_pixels-radius),radius:(N_pixels-radius),None,None] for d in feature]
-        # Then average across all pixels and stack to get big output array
-        hyp_stas = np.stack([np.nansum(sta.reshape([-1, radius*2+1, radius*2+1]), axis=0) for sta in hyp_stas])
-        # Also average the hypothesis itself across all pixels
-        hyp_norm = np.nansum(np.reshape(hyp_rois, [-1, radius*2+1, radius*2+1]),axis=0)
-        # Regress out the contribution of simple hypothesis geometry from responses
-        X = np.stack([np.ones(hyp_norm.size), hyp_norm.reshape(-1)], axis=-1)
-        Y = hyp_stas.reshape([N_features,-1]).T
-        b = np.linalg.pinv(X) @ Y
-        e = Y - X@b
-        corr_stas = e.T.reshape([N_features, radius * 2 + 1, radius * 2 + 1])
-        # Plot for debugging purposes
-        if False:
-            plt.figure();
-            for r in range(8):
-                for c in range(8):
-                    plt.subplot(8,8,r*8+c+1)
-                    plt.imshow(feature[r*8+c])
-            plt.figure();
-            for r in range(8):
-                for c in range(8):
-                    plt.subplot(8,8,r*8+c+1)
-                    plt.imshow(hyp_stas[r*8+c])
-            plt.figure();
-            for r in range(8):
-                for c in range(8):
-                    plt.subplot(8,8,r*8+c+1)
-                    plt.imshow(corr_stas[r*8+c])
-            plt.figure(); plt.imshow(hypothesis[h])
-            plt.figure(); plt.imshow(hyp_norm)       
-            import pdb; pdb.set_trace()             
-        # And append to output stas
-        stas.append(hyp_stas)
-    patch_stas.append(np.stack(stas))
-    
-# Average across patches to get final stas
-all_stas = np.nanmean(np.stack(patch_stas), axis=0)
-# Save as npy file
-np.save('../outputs/sta_dat.npy', all_stas)
+### LOAD OR RUN ANALYSIS ###
 
-# Plot a selection
+# Load stas and fits, or recalculate them
+load = False
+base_dir = '../outputs/radius05'
+if load:
+    all_stas = np.load(os.path.join(base_dir, 'sta_dat.npy'))
+    all_fits = np.load(os.path.join(base_dir, 'sta_fit.npy'))
+    all_params = np.load(os.path.join(base_dir, 'sta_par.npy'))
+else:
+    # Run through all patches, collecting spike triggered averages for each feature for each hypothesis
+    patch_stas = []
+    for p, (hypothesis, feature) in enumerate(zip(hypotheses, features)):
+        print(f'Analysing patch {p+1} / {len(hypotheses)}')
+        
+        # Create empty region of interest maps: area around each pixel for each band
+        rois = np.full([N_hypotheses, N_pixels - 2 * radius, N_pixels - 2 * radius,
+                        radius * 2 + 1, radius * 2 + 1], np.nan)
+        
+        # Collect searchlight data for each pixel from all bands of current modality
+        for h, hyp in enumerate(hypothesis):
+            print(f'Copying hyp {h} / {len(hypotheses)}')
+            for row in range(radius, N_pixels - radius):
+                for col in range(radius, N_pixels - radius):
+                    # Grab the relevant pixels from the band
+                    rois[h, row - radius, col - radius, :, :] = \
+                        hyp[(row - radius):(row + radius + 1), (col - radius):(col + radius + 1)]
+                        
+        # Collect spike time averages for each band
+        stas = []
+        for h, hyp_rois in enumerate(rois):
+            print(f'Calculating spike triggered averages for hyp {h} / {len(rois)}')
+            # Then create the spike time average: multiply each roi by the pixel value of the feature pixel
+            hyp_stas = [hyp_rois * d[radius:(N_pixels-radius),radius:(N_pixels-radius),None,None] for d in feature]
+            # Then average across all pixels and stack to get big output array
+            hyp_stas = np.stack([np.nansum(sta.reshape([-1, radius*2+1, radius*2+1]), axis=0) for sta in hyp_stas])
+            # Also average the hypothesis itself across all pixels
+            hyp_norm = np.nansum(np.reshape(hyp_rois, [-1, radius*2+1, radius*2+1]),axis=0)
+            # Regress out the contribution of simple hypothesis geometry from responses
+            X = np.stack([np.ones(hyp_norm.size), hyp_norm.reshape(-1)], axis=-1)
+            Y = hyp_stas.reshape([N_features,-1]).T
+            b = np.linalg.pinv(X) @ Y
+            e = Y - X@b
+            corr_stas = e.T.reshape([N_features, radius * 2 + 1, radius * 2 + 1])
+            # Plot for debugging purposes
+            if False:
+                plt.figure();
+                for r in range(8):
+                    for c in range(8):
+                        plt.subplot(8,8,r*8+c+1)
+                        plt.imshow(feature[r*8+c])
+                plt.figure();
+                for r in range(8):
+                    for c in range(8):
+                        plt.subplot(8,8,r*8+c+1)
+                        plt.imshow(hyp_stas[r*8+c])
+                plt.figure();
+                for r in range(8):
+                    for c in range(8):
+                        plt.subplot(8,8,r*8+c+1)
+                        plt.imshow(corr_stas[r*8+c])
+                plt.figure(); plt.imshow(hypothesis[h])
+                plt.figure(); plt.imshow(hyp_norm)       
+                import pdb; pdb.set_trace()             
+            # And append to output stas
+            stas.append(hyp_stas)
+        patch_stas.append(np.stack(stas))
+        
+    # Average across patches to get final stas
+    all_stas = np.nanmean(np.stack(patch_stas), axis=0)
+    # Save as npy file
+    np.save(os.path.join(base_dir, 'sta_dat.npy'), all_stas)
+    
+    # Fit a gaussian to each sta
+    x, y = np.meshgrid(np.arange(2*radius+1), np.arange(2*radius+1))
+    # Collect fitted parameters and resulting images
+    all_params = np.full(list(all_stas.shape[:-2]) + [7], np.nan)
+    all_fits = np.full(all_stas.shape, np.nan)
+    for row, hyp_stas in enumerate(all_stas):
+        print(f'Fitting Gaussians to hyp {row} / {N_hypotheses}')
+        for col, sta in enumerate(hyp_stas):
+            curr_fits = []
+            curr_pars = []
+            for fit_type in ['pos', 'neg']:
+                # Set fit type dependent initial guesses: amplitude, offset, center
+                if fit_type == 'pos':       
+                    a_0 = np.max(sta) - np.min(sta)
+                    (y_0, x_0) = np.unravel_index(sta.argmax(), sta.shape)
+                    o_0 = np.min(sta)
+                else:
+                    a_0 = np.min(sta) - np.max(sta)
+                    (y_0, x_0) = np.unravel_index(sta.argmin(), sta.shape)
+                    o_0 = np.max(sta)
+                # Estimate the std as the pixel distance between the peak and the inflexion point:
+                # The location where the second derivative is nearest to 0
+                curve = np.mean(sta, axis=0)
+                curve = np.diff(np.diff(np.mean(sta, axis=0)))
+                sx_0 = max(1e-3, np.abs(x_0 - np.argmin(np.abs(np.diff(np.diff(np.mean(sta, axis=0))))) - 1)) # -1 because diff loses dim
+                sy_0 = max(1e-3, np.abs(y_0 - np.argmin(np.abs(np.diff(np.diff(np.mean(sta, axis=1))))) - 1))
+                theta_0 = 0.0
+                # Do the actual fit
+                try:
+                    # find the optimal Gaussian parameters
+                    popt, pcov = opt.curve_fit(gauss_2d, (x, y), sta.ravel(), 
+                                               p0=(a_0, x_0, y_0, sx_0, sy_0, theta_0, o_0),
+                                               maxfev=int(1e5))   
+                    # Store the resulting parameters and fit
+                    curr_fits.append(gauss_2d((x, y), *popt).reshape(2*radius+1,2*radius+1))
+                    curr_pars.append(popt)
+                except RuntimeError as e:
+                    # This generally happens when we run out of iterations
+                    # That's usually caused by strongly non-gaussian stas. Let's just ignore those
+                    print(f'Gaussian fit failed for type {fit_type}, hyp {row}, feature {col}. \n Error message: {e}')
+                    # Store the resulting parameters and fit
+                    curr_fits.append(np.zeros_like(sta))
+                    curr_pars.append(np.zeros(7))
+            # Only keep the best fit, between the positive and negative peak
+            best_fit = int(np.sum(np.square(sta - curr_fits[0])) > np.sum(np.square(sta - curr_fits[1])))
+            # Keep the best fit between min and max
+            all_params[row, col, :] = curr_pars[best_fit]
+            all_fits[row, col, :, :] = curr_fits[best_fit]
+    
+    # Save as npy file
+    np.save(os.path.join(base_dir, 'sta_fit.npy'), all_fits)
+    np.save(os.path.join(base_dir, 'sta_par.npy'), all_params)
+
+### PLOT RESULTS ###
+
+# Plot a selection of stas
 h_to_plot = len(all_stas)
 f_to_plot = N_features
 plt.figure(figsize=(f_to_plot, h_to_plot))
@@ -147,12 +215,13 @@ for row, hyp_stas in enumerate(all_stas[:h_to_plot]):
             ax.set_ylabel(names[row].replace('_','\n'), rotation=0, labelpad=20)
         if row == 0:
             ax.set_title(f'F{col}')
-plt.savefig('../outputs/sta_dat.png')
+plt.tight_layout();
+plt.savefig(os.path.join(base_dir, 'sta_dat.png'))
             
 # Plot the map for a particular feature that you might like across patches
-cols=4
+cols=10
 rows=int(np.ceil(patches/cols))
-curr_f = 12
+curr_f = 35
 lim = np.nanmax(np.abs(np.stack(features, axis=-1)[curr_f]))
 plt.figure(figsize=(2*cols, 2*rows))
 for p, feature in enumerate(features):
@@ -160,60 +229,7 @@ for p, feature in enumerate(features):
     plt.imshow(feature[curr_f], vmin=-lim, vmax=lim, cmap="RdBu_r")
     plt.axis('off')
 
-# Fit a gaussian to each sta
-x, y = np.meshgrid(np.arange(2*radius+1), np.arange(2*radius+1))
-# Collect fitted parameters and resulting images
-all_params = np.full(list(all_stas.shape[:-2]) + [7], np.nan)
-all_fits = np.full(all_stas.shape, np.nan)
-for row, hyp_stas in enumerate(all_stas[:h_to_plot]):
-    print(f'Fitting Gaussians to hyp {row} / {N_hypotheses}')
-    for col, sta in enumerate(hyp_stas[:f_to_plot]):
-        curr_fits = []
-        curr_pars = []
-        for fit_type in ['pos', 'neg']:
-            # Set fit type dependent initial guesses: amplitude, offset, center
-            if fit_type == 'pos':       
-                a_0 = np.max(sta) - np.min(sta)
-                (y_0, x_0) = np.unravel_index(sta.argmax(), sta.shape)
-                o_0 = np.min(sta)
-            else:
-                a_0 = np.min(sta) - np.max(sta)
-                (y_0, x_0) = np.unravel_index(sta.argmin(), sta.shape)
-                o_0 = np.max(sta)
-            # Estimate the std as the pixel distance between the peak and the inflexion point:
-            # The location where the second derivative is nearest to 0
-            curve = np.mean(sta, axis=0)
-            curve = np.diff(np.diff(np.mean(sta, axis=0)))
-            sx_0 = max(1e-3, np.abs(x_0 - np.argmin(np.abs(np.diff(np.diff(np.mean(sta, axis=0))))) - 1)) # -1 because diff loses dim
-            sy_0 = max(1e-3, np.abs(y_0 - np.argmin(np.abs(np.diff(np.diff(np.mean(sta, axis=1))))) - 1))
-            theta_0 = 0.0
-            # Do the actual fit
-            try:
-                # find the optimal Gaussian parameters
-                popt, pcov = opt.curve_fit(gauss_2d, (x, y), sta.ravel(), 
-                                           p0=(a_0, x_0, y_0, sx_0, sy_0, theta_0, o_0),
-                                           maxfev=int(1e5))   
-                # Store the resulting parameters and fit
-                curr_fits.append(gauss_2d((x, y), *popt).reshape(2*radius+1,2*radius+1))
-                curr_pars.append(popt)
-            except RuntimeError as e:
-                # This generally happens when we run out of iterations
-                # That's usually caused by strongly non-gaussian stas. Let's just ignore those
-                print(f'Gaussian fit failed for type {fit_type}, hyp {row}, feature {col}. \n Error message: {e}')
-                # Store the resulting parameters and fit
-                curr_fits.append(np.zeros_like(sta))
-                curr_pars.append(np.zeros(7))
-        # Only keep the best fit, between the positive and negative peak
-        best_fit = int(np.sum(np.square(sta - curr_fits[0])) > np.sum(np.square(sta - curr_fits[1])))
-        # Keep the best fit between min and max
-        all_params[row, col, :] = curr_pars[best_fit]
-        all_fits[row, col, :, :] = curr_fits[best_fit]
-
-# Save as npy file
-np.save('../outputs/sta_fit.npy', all_fits)
-np.save('../outputs/sta_par.npy', all_params)
-
-# Plot a selection
+# Plot fits for the same stas
 plt.figure(figsize=(f_to_plot, h_to_plot))
 # Color plot borders by average value
 lim = np.nanmax(np.abs(all_fits[:h_to_plot, :f_to_plot]))
@@ -233,4 +249,28 @@ for row, hyp_fits in enumerate(all_fits[:h_to_plot]):
             ax.set_ylabel(names[row].replace('_','\n'), rotation=0, labelpad=20)
         if row == 0:
             ax.set_title(f'F{col}')
-plt.savefig('../outputs/sta_fit.png')            
+plt.tight_layout();            
+plt.savefig(os.path.join(base_dir, 'sta_fit.png'))         
+
+# Make overviews of all parameter histograms
+plot_params = np.copy(all_params)
+plot_params[:,:,1] -= radius
+plot_params[:,:,2] -= radius
+param_names = ['Amplitude', 'Center x', 'Center y', 'Sigma x', 'Sigma y', 'Angle', 'Base'];
+param_N_bins = 20;
+param_bins = [np.linspace(np.percentile(plot_params[:,:,p], 10),
+                          np.percentile(plot_params[:,:,p], 90), param_N_bins) 
+              for p in range(plot_params.shape[-1])]
+plt.figure(figsize=(2*plot_params.shape[-1], plot_params.shape[0]))
+for h, hyp_par in enumerate(plot_params):
+    for p, (p_name, p_bins) in enumerate(zip(param_names, param_bins)):
+        ax = plt.subplot(plot_params.shape[0], plot_params.shape[-1], h*plot_params.shape[-1] + p + 1)
+        plt.hist(hyp_par[:,p], p_bins)
+        if p == 0:
+            ax.set_ylabel(names[h].replace('_','\n'), rotation=0, labelpad=20)
+        if h == 0:
+            ax.set_title(p_name)
+        if h < plot_params.shape[0] - 1:
+            ax.set_xticks([])
+plt.tight_layout()
+plt.savefig(os.path.join(base_dir, 'sta_par.png'))         
