59f9c9a842e46fcd420d0bd97bf37bd3ad54d42f
Thijs van der Plas <vdplasthijs@gmail.com>
2025-11-10 13:26:53 +0100

    create vis utils

---
+++ src/vis_utils.py
@@ -0,0 +1,220 @@
+import os, sys, json 
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import rasterio, rasterio.plot
+import xarray as xr
+import rioxarray as rxr
+from collections import Counter
+
+
+import loadpaths
+path_dict = loadpaths.loadpaths()
+sys.path.append(os.path.join(path_dict['repo'], 'content/'))
+import data_utils as du
+
+def naked(ax):
+    '''Remove all spines, ticks and labels'''
+    for ax_name in ['top', 'bottom', 'right', 'left']:
+        ax.spines[ax_name].set_visible(False)
+    ax.set_xticks([])
+    ax.set_yticks([])
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+
+def despine(ax):
+    for sp in ['top', 'right']:
+        ax.spines[sp].set_visible(False)
+
+def plot_image_simple(im, ax=None, name_file=None, use_im_extent=False, verbose=0):
+    '''Plot image (as np array or xr DataArray)'''
+    if ax is None:
+        ax = plt.subplot(111)
+    if type(im) == xr.DataArray:
+        plot_im = im.to_numpy()
+    else:
+        plot_im = im
+    if verbose > 0:
+        print(plot_im.shape, type(plot_im))
+    if use_im_extent:
+        extent = [im.x.min(), im.x.max(), im.y.min(), im.y.max()]
+    else:
+        extent = None
+    rasterio.plot.show(plot_im, ax=ax, cmap='viridis', 
+                       extent=extent)
+    naked(ax)
+    ax.set_aspect('equal')
+    if name_file is None:
+        pass 
+    else:
+        name_tile = name_file.split('/')[-1].rstrip('.tif')
+        ax.set_title(name_tile)
+
+def plot_overview_images(path_folder=path_dict['data_folder'], name='sample-0', 
+                         plot_alphaearth=True, plot_dynamicworld_full=True,
+                         verbose=0):
+    (file_sent, file_alpha, file_dynamic, file_worldclimbio, file_dsm) = du.get_images_from_name(path_folder=path_folder, name=name)
+    path_sent = os.path.join(path_folder, file_sent) if file_sent is not None else None
+    path_alpha = os.path.join(path_folder, file_alpha) if file_alpha is not None else None
+    path_dynamic = os.path.join(path_folder, file_dynamic) if file_dynamic is not None else None
+    path_worldclimbio = os.path.join(path_folder, file_worldclimbio) if file_worldclimbio is not None else None
+    path_dsm = os.path.join(path_folder, file_dsm) if file_dsm is not None else None
+
+    if path_alpha is not None:
+        im_loaded_alpha = du.load_tiff(path_alpha, datatype='da')
+        im_plot_alpha = im_loaded_alpha
+        ## normalise:
+        im_plot_alpha.values[im_plot_alpha.values == -np.inf] = np.nan
+        im_plot_alpha.values[im_plot_alpha.values == np.inf] = np.nan
+        im_plot_alpha = (im_plot_alpha - np.nanmin(im_plot_alpha)) / (np.nanmax(im_plot_alpha) - np.nanmin(im_plot_alpha))
+        # im_plot_alpha = np.swapaxes(im_plot_alpha, 2, 1)  # change to (bands, height, width) to (height, width, bands)`
+
+    if path_sent is not None:
+        im_loaded_s2 = du.load_tiff(path_sent, datatype='da')
+        im_loaded_s2 = np.clip(im_loaded_s2, 0, 3000)
+        im_loaded_s2 = im_loaded_s2 / (3000)
+        im_plot_s2 = im_loaded_s2[:3, ...]
+        # im_nir_s2 = im_loaded_s2[1:, ...]
+        im_nir_s2 = im_loaded_s2[np.array([0, 1, 3]), ...]  # B4, B3, B8
+        ## put B8 band first:
+        im_nir_s2 = im_nir_s2[[2, 0, 1], ...]
+        size_s2 = im_plot_s2.shape[1]
+        assert size_s2 == im_plot_s2.shape[2]
+        half_size_s2 = size_s2 // 2
+        quarter_size_s2 = size_s2 // 4
+        # im_plot_s2 = im_plot_s2[:, quarter_size_s2:half_size_s2 + quarter_size_s2, quarter_size_s2:half_size_s2 + quarter_size_s2]
+        
+    if path_dynamic is not None:
+        im_loaded_dynamic = du.load_tiff(path_dynamic, datatype='da')
+        im_argmax_dynamic = np.argmax(im_loaded_dynamic.values, axis=0)
+        print('LC pixel count:', Counter(im_argmax_dynamic.flatten()))
+        ## normalise:
+        # im_plot_dynamic.values[im_plot_dynamic.values == -np.inf] = np.nan
+        # im_plot_dynamic.values[im_plot_dynamic.values == np.inf] = np.nan
+        # im_plot_dynamic = (im_plot_dynamic - np.nanmin(im_plot_dynamic)) / (np.nanmax(im_plot_dynamic) - np.nanmin(im_plot_dynamic))
+
+    if path_dsm is not None:
+        im_loaded_dsm = du.load_tiff(path_dsm, datatype='da')
+        
+    if verbose:
+        print(im_loaded_alpha.shape, type(im_loaded_alpha))
+        print(im_loaded_s2.shape, type(im_loaded_s2))
+
+    n_rows = 1 + 2 * int(plot_dynamicworld_full) + 4 * int(plot_alphaearth)
+
+    fig, ax = plt.subplots(n_rows, 5, figsize=(15, 3 * n_rows))
+    ax = ax.flatten()
+
+    ## Top row:
+    plot_image_simple(im_plot_s2, ax=ax[0])
+    ax[0].set_title('Sentinel-2 RGB')
+
+    plot_image_simple(im_nir_s2, ax=ax[1])
+    ax[1].set_title('Sentinel-2 near infrared')
+
+    if path_dynamic is not None:
+        dict_classes = du.create_cmap_dynamic_world()
+        cmap_dw = ListedColormap([v for v in dict_classes.values()])
+        im = ax[2].imshow(im_argmax_dynamic, cmap=cmap_dw, interpolation='none', origin='upper', vmax=8.5, vmin=-0.5)
+        # Place colorbar outside of ax[2] to avoid shrinking the imshow
+        cbar = fig.colorbar(im, ax=ax[2], ticks=np.arange(0, 9), location='right', fraction=0.046, pad=0.04)
+        cbar.ax.set_yticks(np.arange(0, 9))
+        cbar.ax.set_yticklabels([k for k in dict_classes.keys()])
+        # ax[2].set_title('Dynamic World land cover')
+
+    if path_dsm is not None:
+        plot_image_simple(im_loaded_dsm, ax=ax[4], name_file=path_dsm)
+        ## cbar:
+        mappable = ax[4].images[0]
+        cbar = fig.colorbar(mappable, ax=ax[4], location='right', fraction=0.046, pad=0.04)
+        ax[4].set_title('DSM (m)')
+
+    for ii in range(2, 5):
+        naked(ax[ii])
+
+    ## dynamic world full:
+    if plot_dynamicworld_full and path_dynamic is not None:
+        for ii in range(9):
+            ax_ind = 5 + ii
+            im = ax[ax_ind].imshow(im_loaded_dynamic[ii, ...], cmap='viridis', interpolation='none', 
+                                   origin='upper', vmin=0, vmax=1)
+            naked(ax[ax_ind])
+            ax[ax_ind].set_title(f'DW {ii}: {list(dict_classes.keys())[ii]}')
+        ## add cbar to last plot:
+        cbar = fig.colorbar(im, ax=ax[ax_ind], ticks=np.linspace(0, 1, 6),
+                            location='right', fraction=0.046, pad=0.04)
+        cbar.ax.set_ylabel('Probability', rotation=270, labelpad=15)
+        ax_ind += 1
+        naked(ax[ax_ind])
+
+    ## alpha earth:
+    if plot_alphaearth and path_alpha is not None:
+        for ii in range(20):
+            ax_ind += 1
+            bands_alpha_plot = np.arange(ii * 3, (ii + 1) * 3)
+            if bands_alpha_plot.max() >= im_plot_alpha.shape[0]:
+                if ax_ind < len(ax):
+                    naked(ax[ax_ind])
+                continue
+            plot_image_simple(im_plot_alpha[bands_alpha_plot, ...], ax=ax[ax_ind])
+            ax[ax_ind].set_ylim(ax[ax_ind].get_ylim()[::-1])
+            ax[ax_ind].set_title(f'AlphaEarth bands {bands_alpha_plot}')
+
+
+def plot_distr_embeddings(path_folder=path_dict['data_folder'], name='sample-0', verbose=0):
+    (file_sent, file_alpha, file_dynamic, file_worldclimbio, file_dsm) = du.get_images_from_name(path_folder=path_folder, name=name)
+    path_sent = os.path.join(path_folder, file_sent) if file_sent is not None else None
+    path_alpha = os.path.join(path_folder, file_alpha) if file_alpha is not None else None
+
+    if path_alpha is not None:
+        im_loaded_alpha = du.load_tiff(path_alpha, datatype='da')
+        im_plot_alpha = im_loaded_alpha
+        ## normalise:
+        im_plot_alpha.values[im_plot_alpha.values == -np.inf] = np.nan
+        im_plot_alpha.values[im_plot_alpha.values == np.inf] = np.nan
+        im_plot_alpha = (im_plot_alpha - np.nanmin(im_plot_alpha)) / (np.nanmax(im_plot_alpha) - np.nanmin(im_plot_alpha))
+        # im_plot_alpha = np.swapaxes(im_plot_alpha, 2, 1)  # change to (bands, height, width) to (height, width, bands)`
+
+    if path_sent is not None:
+        im_loaded_s2 = du.load_tiff(path_sent, datatype='da')
+        im_loaded_s2 = np.clip(im_loaded_s2, 0, 3000)
+        im_loaded_s2 = im_loaded_s2 / (3000)
+        im_plot_s2 = im_loaded_s2[:3, ...]
+        im_nir_s2 = im_loaded_s2[1:, ...]
+        size_s2 = im_plot_s2.shape[1]
+        assert size_s2 == im_plot_s2.shape[2]
+        half_size_s2 = size_s2 // 2
+        quarter_size_s2 = size_s2 // 4
+        # im_plot_s2 = im_plot_s2[:, quarter_size_s2:half_size_s2 + quarter_size_s2, quarter_size_s2:half_size_s2 + quarter_size_s2]
+        
+    if verbose:
+        print(im_loaded_alpha.shape, type(im_loaded_alpha))
+        print(im_loaded_s2.shape, type(im_loaded_s2))
+
+    fig, ax = plt.subplots(3, 3, figsize=(10, 10))
+    ax = ax.flatten()
+
+    plot_image_simple(im_plot_s2, ax=ax[0])
+    ax[0].set_title('Sentinel-2 RGB')
+
+    for ii in range(8):
+        bands = np.arange(ii * 8, (ii + 1) * 8)
+        if bands.max() >= im_plot_alpha.shape[0]:
+            if ii + 1 < len(ax):
+                naked(ax[ii + 1])
+            continue
+
+        ax_ind = ii + 1
+        curr_ax = ax[ax_ind]
+        for jj, band in enumerate(bands):
+            if band >= im_plot_alpha.shape[0]:
+                continue
+            curr_ax.hist(im_plot_alpha[band, ...].to_numpy().flatten(), bins=np.linspace(0, 1, 41), alpha=0.5, label=f'Band {band}',
+                         histtype='stepfilled', density=True, color=plt.cm.viridis(jj / len(bands)))
+        curr_ax.set_xlim(-0.1, 1.1)
+
+def plot_sent_feat(sentinel_patch, ax=None):
+    if ax is None:
+        ax = plt.subplot(111)
+    ax.imshow(np.clip(np.swapaxes(np.swapaxes(sentinel_patch[:3], 0, 2), 0, 1), 0, 3000) / 3000)
